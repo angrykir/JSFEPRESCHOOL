6e84103f981d053916284cc7696669b22d8c577d
Kirill Maksimuk <angrykir@gmail.com>
2022-02-01 00:15:26 +0300

    feat: change images in portfolio

--- portfolio/index.js
+++ portfolio/index.js
@@ -10,4 +10,35 @@ function closeMenu(event) {
     if (event.target.classList.contains('slide-link')) {
         document.getElementById("slide-menu-checkbox").checked = false;
     }
-  };
+};
+
+// Changing portfolio images
+
+const portfolioBtn = document.querySelector('.portfolio-btn');
+const portfolioBtns = document.querySelector('.portfolio-btns');
+const portfolioBtnArr = document.querySelectorAll('.portfolio-btn');
+const portfolioImages = document.querySelectorAll('.portfolio-image');
+
+function changeImage(event) {
+    const season = event.target.dataset.season;
+    console.log(season);
+    if (event.target.classList.contains('portfolio-btn')) {
+        portfolioImages.forEach((img, index) => img.src = `./assets/img/${season}/${index + 1}.jpg`);
+    }
+};
+portfolioBtns.addEventListener('click', changeImage);
+
+// Add class active
+
+function changeClassActive(event) {
+    if (event.target.classList.contains('portfolio-btn')) {
+        for (let i = 0; i < portfolioBtnArr.length; i++) {
+            portfolioBtnArr[i].classList.remove('active');
+        }
+    }
+    event.target.classList.add('active');
+};
+portfolioBtns.addEventListener('click', changeClassActive);
+
+
+
