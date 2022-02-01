fc3f9748434f2b01bc83519944883aa72f3a25da
Kirill Maksimuk <angrykir@gmail.com>
2022-02-01 04:27:15 +0300

    feat: change languages

--- portfolio/index.js
+++ portfolio/index.js
@@ -14,31 +14,74 @@ function closeMenu(event) {
 
 // Changing portfolio images
 
-const portfolioBtn = document.querySelector('.portfolio-btn');
 const portfolioBtns = document.querySelector('.portfolio-btns');
 const portfolioBtnArr = document.querySelectorAll('.portfolio-btn');
 const portfolioImages = document.querySelectorAll('.portfolio-image');
 
 function changeImage(event) {
-    const season = event.target.dataset.season;
-    console.log(season);
     if (event.target.classList.contains('portfolio-btn')) {
-        portfolioImages.forEach((img, index) => img.src = `./assets/img/${season}/${index + 1}.jpg`);
+        portfolioImages.forEach((img, index) => img.src = `./assets/img/${event.target.dataset.season}/${index + 1}.jpg`);
     }
 };
 portfolioBtns.addEventListener('click', changeImage);
 
-// Add class active
+// Add class active portfolio
 
-function changeClassActive(event) {
+function changeClassActivePortfolio(event) {
     if (event.target.classList.contains('portfolio-btn')) {
-        for (let i = 0; i < portfolioBtnArr.length; i++) {
-            portfolioBtnArr[i].classList.remove('active');
-        }
+        portfolioBtnArr.forEach((element) => element.classList.remove('active'));
     }
     event.target.classList.add('active');
 };
-portfolioBtns.addEventListener('click', changeClassActive);
+portfolioBtns.addEventListener('click', changeClassActivePortfolio);
+
+//Сaching images
+
+// const seasons = ['winter', 'spring', 'summer', 'autumn'];
+
+// function preloadSummerImages() {
+//     for(let i = 1; i <= 6; i++) {
+//       const img = new Image();
+//       img.src = `./assets/img/summer/${i}.jpg`;
+//     }
+//   }
+//   preloadSummerImages();
+
+//Translate page
+
+import i18Obj from './translate.js';
+
+const ruLanguage = document.querySelector('.ru-language');
+const enLanguage = document.querySelector('.en-language');
+
+function getTranslate(language) {
+    language.target === ruLanguage ? language = 'ru' : language = 'en'; 
+    let dataI18Arr = document.querySelectorAll('[data-i18]');
+    dataI18Arr.forEach((element) => {
+        if (element.placeholder) {
+            element.placeholder = i18Obj[language][element.dataset.i18];
+            element.textContent = ''
+        }
+        element.textContent = i18Obj[language][element.dataset.i18];
+    });
+};
+
+ruLanguage.addEventListener('click', getTranslate);
+enLanguage.addEventListener('click', getTranslate);
+
+// Add class active language
+
+const langBtns = document.querySelector('.lang');
+const langBtnsArr = document.querySelectorAll('.lang-link');
+
+function changeClassActiveLang(event) {
+    if (event.target.classList.contains('lang-link')) {
+        langBtnsArr.forEach((element) => element.classList.remove('active'));
+    }
+    event.target.classList.add('active');
+};
+langBtns.addEventListener('click', changeClassActiveLang);
 
 
+window.alert("\nПроверьте пожалуйста работу в среду. Немного не успел, в процессе.\n\n┬┴┬┴┤( ͡° ͜ʖ├┬┴┬┴");
 
