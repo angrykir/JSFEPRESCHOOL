6012f80ee30fd1d3a4907c21b8c9f5ac2bb0de04
Kirill Maksimuk <angrykir@gmail.com>
2022-02-08 03:01:44 +0300

    fix: renew design

--- audio-player/playlist.js
+++ audio-player/playlist.js
@@ -1,15 +1,39 @@
 const playlist = {
     track1: {
-        'audio' : './assets/audio/beyonce.mp3',
-        'track-name' : 'Dont Hurt Yourself',
-        'track-artist' : 'Beyonce',
-        'cover' : './assets/img/lemonade.png',
+        'audio' : './assets/audio/Never Going Home.mp3',
+        'track-name' : 'Never Going Home',
+        'track-artist' : 'Kungs',
+        'cover' : './assets/img/NeverGoingHome.jfif',
     },
-    track2: {
-        'audio' : './assets/audio/dontstartnow.mp3',
-        'track-name' : 'Dont Start Now',
-        'track-artist' : 'Dua Lipa',
-        'cover' : './assets/img/dontstartnow.png',
+     track2: {
+        'audio' : './assets/audio/Terminator 2 Theme.mp3',
+        'track-name' : 'Terminator 2 Theme',
+        'track-artist' : 'Brad Fiedel',
+        'cover' : './assets/img/Terminator2Theme.jfif',
+    },
+    track3: {
+        'audio' : './assets/audio/Dont Stop Believin.mp3',
+        'track-name' : 'Dont Stop Believin',
+        'track-artist' : 'Journey',
+        'cover' : './assets/img/DontStopBelievin.jfif',
+    },
+    track4: {
+        'audio' : './assets/audio/Hit That.mp3',
+        'track-name' : 'Hit That',
+        'track-artist' : 'The Offspring',
+        'cover' : './assets/img/HitThat.jfif',
+    },
+    track5: {
+        'audio' : './assets/audio/Cheri Cheri Lady.mp3',
+        'track-name' : 'Modern Talking',
+        'track-artist' : 'Brad Fiedel',
+        'cover' : './assets/img/CheriCheriLady.jfif',
+    },
+    track6: {
+        'audio' : './assets/audio/Take on Me.mp3',
+        'track-name' : 'Take on Me',
+        'track-artist' : 'a-ha',
+        'cover' : './assets/img/TakeonMe.jfif',
     }
 }
 export default playlist;
